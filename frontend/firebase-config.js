4f72a39e2e7e8a14b7368d4d8a592d8644f0d9e6
Rohit Saxena <34920137+rohit290554@users.noreply.github.com>
2025-12-27 17:32:43 +0530

    Create firebase-config.js

---
+++ frontend/firebase-config.js
@@ -0,0 +1,7 @@
+// Firebase Web Config (PUBLIC – safe to expose)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDwDNkR0AtuHB5SMZJivNRQ8h_caU_FmBw",
+  authDomain: "findmobo-200404.firebaseapp.com",
+  projectId: "findmobo-200404"
+};
